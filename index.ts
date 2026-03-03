244b7d039f675cd6fcf4e5417233fb0dc21bd810
lancast <angelprep92@gmail.com>
2026-03-03 16:33:08 -0600

    prueba husky

--- index.ts
+++ index.ts
@@ -1,2 +1,14 @@
 console.log('Hello via Bun!');
 console.log(process.version);
+console.log('hola perrita');
+function suma(a: number, b: number): number {
+  return a + b;
+}
+
+const resultado = suma(2, 3);
+
+if (resultado !== 5) {
+  throw new Error("La suma está mal ❌");
+}
+
+console.log("Todo correcto ✅");
